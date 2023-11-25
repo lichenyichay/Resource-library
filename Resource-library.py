2182259035ef7a87eae103cc555846d21c9fe968
chay <lichenyi_2020@qq.com>
2023-11-25 08:27:28 +0800

    Add the menu.py and Resource-library.py
    新增menu.py和Resource-library.py

---
+++ Resource-library.py
@@ -0,0 +1,190 @@
+from menu import *
+import os,webbrowser,time
+while True:
+    os.system("cls")
+    showmain()
+    a = input("1-资源包\n2-编程必备软件安装包\n3-电脑插件\n4-退出\n请输入前面的序号：")
+    if a == "1":
+        while True:
+            os.system("cls")
+            input1()
+            d = input("1-电脑资源包\n2-开发资源包\n3-返回上一级\n请输入前面的序号：")
+            if d == "1":
+                while True:
+                    os.system("cls")
+                    input2()
+                    b = input("1-office\n2-返回上一级\n请输入前面的序号：")
+                    if b == "1":
+                        while True:
+                            os.system("cls")
+                            input21()
+                            c = input("1-宇哥玩Access示例\n2-新手学电脑从入门到精通：Windows 10+Word／Excel／PPT 2016\n3-Excel技巧视频81个\n4-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                # 打开百度网盘
+                                print("提取码（四位）：数据恢复中一个错误码（虽然提取出来了，但无法使用，字母大写，不知道的看评论区）")# 提取码: 241A
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1VSXpJWetXGVcEEQZPWpZcA")
+                            elif c == "2":
+                                print("提取码（四位）：office年份版最新的版本（数字）")# 提取码: 2019
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/11WIBHg41tKI3bNzClHS7ww")
+                            elif c == "3":
+                                print("提取码（四位）：str(5²) * 2")# 提取码: 2525
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1avxbCqSwVeu1Unz-wFcGrg")
+                            elif c == "4":
+                                break
+                            else:
+                                print("序号无效！")
+                        break
+                    elif b == "2":
+                        break
+                    else:
+                        print("序号无效！")
+            elif d == "2":
+                while True:
+                    os.system("cls")
+                    input3()
+                    b = input("1-C++&C\n2-Python\n3-iOS\n4-Android\n5-Java&Kotlin\n6-其他\n7-返回上一级\n请输入前面的序号：")
+                    if b == "1":
+                        while True:
+                            os.system("cls")
+                            input31()
+                            c = input("1-《C++从入门到精通（项目案例版）》\n2-《C语言从入门到精通（项目案例版）》\n3-《Visual C++从入门到精通（项目案例版）》\n4-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                print("提取码（四位）：82²") # 提取码: 6724
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1KA6DMFIX_V-0SOdkv6TvFw")
+                            elif c == "2":
+                                print("提取码（四位）：91**4//10000") # 提取码: 6857
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1cIgB18LNhlpXvoCH2xBaOg")
+                            elif c == "3":
+                                print("提取码（四位）：91²") # 提取码: 8281
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1QhTz-nw_upccQr4BiBLNBw")
+                            elif c == "4":
+                                break
+                            else:
+                                print("序号无效！")
+                    elif b == "2":
+                        while True:
+                            os.system("cls")
+                            input32()
+                            c = input("1-《超简单：用Python让Excel飞起来》系列\n2-《从scratch到python轻松学》\n3-《案例学Python（进阶篇）》\n4-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                print("提取码（四位）：91²") # 提取码: 8281
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1gfOidtsobVxKAMY1HhoyMw")
+                            elif c == "2":
+                                # 打开百度网盘
+                                print("提取码（四位）：abs((-10)+(-20)+(-30)+...+(-100))")# 提取码: 0550
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/15DW-wKkEaVtJendow-b7UA")
+                            elif c == "3":
+                                
+                            elif c == "4":
+                                break
+                            else:
+                                print("序号无效！")
+                    elif b == "3":
+                        while True:
+                            os.system("cls")
+                            input33()
+                            c = input("1-iOS 8\n2-iOS-9\n3-iOS 10\n4-iOS 11\n5-iOS 14\n6-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                print("提取码（四位）：74²") # 提取码: 5476
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1CvJtfRJvECXWc-izZj-LIw")
+                            elif c == "2":
+                                print("提取码（四位）：82**0") # 提取码: 0001
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1_XkYdMFrg7jO2iEYhccJeA")
+                            elif c == "3":
+                                print("提取码（四位）：78²*75//70") # 提取码: 6518
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1ojzCgPI0gU_ZiMsJU9VU4w")
+                            elif c == "4":
+                                print("提取码（四位）：91³//100") # 提取码: 7535
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/15wmghYc53p0OoIO9sLfO5w ")
+                            elif c == "5":
+                                print("提取码（四位）：91**5//1000000") # 提取码: 6240
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/18AZ8WTiuonou9nGr8iJmSw")
+                            elif c == "6":
+                                break
+                            else:
+                                print("序号无效！")
+                    elif b == "4":
+                        while True:
+                            os.system("cls")
+                            input34()
+                            c = input("1-《第一行代码Android》系列\n2-《Android从入门到精通（项目案例版）》\n3-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                print("提取码（四位）：69²") # 提取码: 4761
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/17VCj64tGTrxapY87cliy0Q")
+                            elif c == "2":
+                                print("提取码（四位）：74²") # 提取码: 5476
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/142KlgI1jABay_VjZWP7dgw")
+                            elif c == "3":
+                                break
+                            else:
+                                print("序号无效！")
+                    elif b == "5":
+                        while True:
+                            os.system("cls")
+                            input35()
+                            c = int(input("1-《Java从入门到精通（项目案例版）》\n2-《JavaSE 12基础》\n3-《Kotlin进阶实战》\n4-返回上一级\n请输入前面的序号："))
+                            if c == 1:
+                                print("提取码（四位）：91²*2//10") # 提取码: 1656
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1ogDK1djodaYKBYjfV64-Hw")
+                            elif c == 2:
+                                print("提取码（四位）：88²*2//10") # 提取码: 1548
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/10Qo3RScYHroHXJ9sLrf1ow")
+                            elif c == 3:
+                                print("提取码（四位）：66²*2//10") # 提取码: 8712
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1hZB5bxh8AEJ8lE5sEYP0jg")
+                            elif c == 4:
+                                break
+                            else:
+                                print("序号错误！")
+                    elif b == "6":
+                        while True:
+                            os.system("cls")
+                            input36()
+                            c = input("1-《Visual Basic从入门到精通（项目案例版）》\n3-返回上一级\n请输入前面的序号：")
+                            if c == "1":
+                                print("提取码（四位）：95²") # 提取码: 9025
+                                time.sleep(1)
+                                webbrowser.open("https://pan.baidu.com/s/1HtNXANGUxBeB8RGOJ-9dJg")
+                            elif c == "2":
+                                break
+                            else:
+                                print("序号无效！")
+                    elif b == "7":
+                        break
+                    else:
+                        print("序号无效！")
+            elif d == "3":
+                break
+            else:
+                print("序号无效！")
+    elif a == "2":
+        print("提取码（四位）：三九二十七") # 提取码: 3927
+        time.sleep(1)
+        webbrowser.open("https://pan.baidu.com/s/16e-hbyYniZk-MbR-nZxw2g")
+    elif a == "3":
+        print("提取码（四位）：九九八十一") # 提取码: 9981
+        time.sleep(1)
+        webbrowser.open("https://pan.baidu.com/s/1EfAUoMbHRuHxA4kXzPKzsQ")
+    elif a == "4":
+        print("感谢你的使用，再见！")
+        time.sleep(1)
+        break
+    
